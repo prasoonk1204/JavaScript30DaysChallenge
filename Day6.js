9a92e680688f5281fc94b0409c89433bcecac0d2
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-08-31 20:55:37 +0530

    Create Day6.js

---
+++ Day6.js
@@ -0,0 +1,31 @@
+// task 1
+arr1 = [1,2,3,4,5];
+console.log(arr1);
+
+
+// task 2
+console.log(arr1[0]);
+console.log(arr1[arr1.length-1]);
+
+
+// task 3
+arr1.push(6,7);
+console.log(arr1);
+
+
+// task 4
+arr1.pop();
+console.log(arr1);
+
+
+// task 5
+arr1.shift(arr1[0]);
+console.log(arr1);
+
+
+// task 6
+arr1.unshift(10);
+console.log(arr1);
+
+
+// task 7
